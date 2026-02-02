73d13d37db5f2829580184a45627bae9b1f7011a
parsee <babakovi679@gmail.com>
2026-02-02 21:57:51 +0300

    move to x86

--- src/Renderer/src/renderer.cpp
+++ src/Renderer/src/renderer.cpp
@@ -2,8 +2,47 @@
 #include <cmath>
 #include <iomanip>
 
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl3.h>
+
 #include "renderer.hpp"
 
 using namespace Rndr;
 
 #define INF std::numeric_limits<double>::infinity()
+Rndr::Renderer::~Renderer() {
+    if (window_) {
+        glfwDestroyWindow(window_);
+        window_ = nullptr;
+    }
+    glfwTerminate();
+}
+void Rndr::Renderer::draw() {
+    while (!glfwWindowShouldClose(window_)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(window_);
+        glfwPollEvents();
+    }
+}
+void Rndr::Renderer::initWindow(const std::string &title) {
+    if (glfwInit() == 0) {
+        throw std::runtime_error("Failed to initialize GLFW");
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    window_ =
+        glfwCreateWindow(width_, height_, title.c_str(), nullptr, nullptr);
+    if (!window_) {
+        glfwTerminate();
+        throw std::runtime_error("Failed to create GLFW window");
+    }
+
+    glfwMakeContextCurrent(window_);
+    if (vao_ || vbo_) {
+        return;
+    }
+}
